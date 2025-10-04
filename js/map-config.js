ec3fa58812e03d76e69665098db863b09ee6db2e
SeSo <sobhani.sepehr@gmail.com>
2025-10-04 11:52:17 -0700

    chore: update from main repo oct. 4

--- js/map-config.js
+++ js/map-config.js
@@ -1,5 +1,6 @@
-export const map = L.map('map').setView([53.7267, -127.6476], 6);
+export const map = L.map("map").setView([52.7267, -122.6476], 6);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
